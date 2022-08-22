1e1b6a19a085a7d034ed0b1d69bc80c31b6957da
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-08-22 11:22:39 +0430

    Add files via upload

---
+++ BubbleSort-V1/BubbleSort-V1/Bubble.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort_V1
+{
+    internal class Bubble
+    {
+        public void SortArray(int[] array)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+            foreach (var item in array)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
---
+++ BubbleSort-V1/BubbleSort-V1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort_V1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] b = { 40, 20, 10, 30 };
+            Bubble a = new Bubble();
+            a.SortArray(b);
+            Console.ReadKey();
+            
+        }
+    }
+}
